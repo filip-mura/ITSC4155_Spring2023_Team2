cd49242040e5f5071fae878bfb8550213d9184ec
[AustinAHolmes] <[Aholme36@uncc.edu]>
2023-02-06 23:21:32 -0500

    Testing java class creation and junit creation

---
+++ junit.java
@@ -0,0 +1,6 @@
+public class junit {
+    public static void main(String[] args){
+        System.out.println("Junit Testing");
+    }
+}
+
---
+++ junitTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class junitTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
